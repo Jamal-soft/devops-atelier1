65da5d18a2c0530cc3ed31a2318b6f6ffbe81c32
jamal <jmlaitsalh@gmail.com>
2021-12-18 18:04:00 +0100

    commit changes to test rebase command

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.Println("Hello,Worl!");
+	System.out.Println("commit rebase from scrum master!");
 	}
 
 }
